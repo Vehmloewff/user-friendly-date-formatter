9f75193d6dc8cf29286a3a5e43b85f4925b5966b
Elisarmor <ejsisgreat@gmail.com>
2019-05-28 08:47:30 -0500

    Add a short year (YY)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = (d, format) => {
 	return format
 	.replace(/YYYY/g, d.getFullYear())
+	.replace(/YY/g, String(d.getFullYear()).slice(-2))
 }
--- test.js
+++ test.js
@@ -3,9 +3,15 @@ const df = require('./index');
 
 const d = new Date(1559050001385)
 
-test('Test for year', t => {
+test('Test for YYYY', t => {
 	t.equal(
 		df(d, 'YYYY'),
 		'2019'
+	)
+})
+test('Test for YY', t => {
+	t.equal(
+		df(d, 'YY'),
+		'19'
 	)
 })
