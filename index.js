ebcbfad73575918be42280bb11aad7bb9f6a928f
Elisarmor <ejsisgreat@gmail.com>
2019-05-28 09:06:29 -0500

    Create the months

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports = (d, format) => {
+	monthAbbr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 	return format
 	.replace(/YYYY/g, d.getFullYear())
 	.replace(/YY/g, String(d.getFullYear()).slice(-2))
+	.replace(/MM/g, String(d.getMonth() + 1).length == 1 ? `0${d.getMonth() + 1}` : d.getMonth() + 1 )
+	.replace(/dM/g, d.getMonth() + 1)
+	.replace(/M/g, months[d.getMonth()])
+	.replace(/m/g, monthAbbr[d.getMonth()])
 }
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const { test } = require('zora');
 const df = require('./index');
 
 const d = new Date(1559050001385)
+const later = new Date('2019-12-25');
 
 test('Test for YYYY', t => {
 	t.equal(
@@ -13,5 +14,41 @@ test('Test for YY', t => {
 	t.equal(
 		df(d, 'YY'),
 		'19'
+	)
+})
+test('Test for M', t => {
+	t.equal(
+		df(d, 'M'),
+		'May'
+	)
+	t.equal(
+		df(later, 'M'),
+		'December'
+	)
+})
+test('Test for m', t => {
+	t.equal(
+		df(later, 'm'),
+		'Dec'
+	)
+})
+test('Test for MM', t => {
+	t.equal(
+		df(d, 'MM'),
+		'05'
+	)
+	t.equal(
+		df(later, 'MM'),
+		'12'
+	)
+})
+test('Test for dM', t => {
+	t.equal(
+		df(d, 'dM'),
+		'5'
+	)
+	t.equal(
+		df(later, 'dM'),
+		'12'
 	)
 })
